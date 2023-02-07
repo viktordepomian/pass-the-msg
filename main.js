0df7ca189f4363a7b93228d13ffcf0d590d358ef
viktordepomian <viktor.depomian@gmail.com>
2023-02-07 14:26:49 +0100

    Changes input value

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const outputValue = document.getElementById('outputValue');
 inputValue.addEventListener("change", function() {
   localStorage.setItem("inputValue", inputValue.value);
   outputValue.innerHTML = localStorage.getItem("inputValue");
+  inputValue.value = "";
 });
 
 document.addEventListener("DOMContentLoaded", function() {
   const savedInputValue = localStorage.getItem("inputValue");
   if (savedInputValue) {
-    inputValue.value = savedInputValue;
     outputValue.innerHTML = savedInputValue;
   }
+
 });
+
